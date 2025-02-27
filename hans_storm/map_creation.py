fb94f57f0a4c2b14d7280618cfb1b64add02aebe
esauvat <emile.sauvat@proton.me>
2025-02-27 16:13:17 +0100

    ending hans tp daily maps

--- hans_storm/map_creation.py
+++ hans_storm/map_creation.py
@@ -24,22 +24,18 @@ def nbToDate(nb : int) -> str :
 def map_creator(data : Dataset) -> tuple :
 
     projPlane = ccrs.PlateCarree()
+    projMerc = ccrs.Mercator()
 
-    latS = max(45, data['tp24'].GRIB_latitudeOfLastGridPointInDegrees)
-    latN = min(75, data['tp24'].GRIB_latitudeOfFirstGridPointInDegrees)
-    lonW = max(3.1, data['tp24'].GRIB_longitudeOfFirstGridPointInDegrees)
-    lonE = min(31.3, data['tp24'].GRIB_longitudeOfLastGridPointInDegrees)
+    latS = max(57, data['tp24'].GRIB_latitudeOfLastGridPointInDegrees)
+    latN = min(66, data['tp24'].GRIB_latitudeOfFirstGridPointInDegrees)
+    lonW = max(3, data['tp24'].GRIB_longitudeOfFirstGridPointInDegrees)
+    lonE = min(17, data['tp24'].GRIB_longitudeOfLastGridPointInDegrees)
+
     cLat = (latS + latN) / 2
     cLon = (lonW + lonE) / 2
-
-    # cities_mask = cfeature.NaturalEarthFeature(
-    #     'cultural',
-    #     'Populated_places',
-    #     scale = '50m',
-    # )
  
     fig = plt.figure(figsize=(15,10))
-    ax = plt.subplot(1, 1, 1, projection=projPlane)
+    ax = plt.subplot(1, 1, 1, projection=projMerc)
     ax.set_extent([lonW, lonE, latS, latN], crs=projPlane)
     ax.coastlines()
     ax.add_feature(cfeature.STATES, linewidth=0.3, linestyle='--', edgecolor='black')
@@ -47,7 +43,6 @@ def map_creator(data : Dataset) -> tuple :
     ax.add_feature(cfeature.LAKES, alpha=0.5)
     ax.add_feature(cfeature.LAND)
     ax.add_feature(cfeature.RIVERS)
-    # ax.add_feature(cities_mask)
 
     return fig, ax
 
@@ -113,3 +108,7 @@ for number in range(217,223):
     # mapping(pathToFile="/nird/projects/NS9873K/etdu/processed/cf-forsikring/era5/continuous-format/daily/tp24/tp24_0.25x0.25_2023.nc", day=number)
     mapping_1d(pathToFile="unseen-storm-forecasts/hans_storm/tp24_0.25x0.25_2023.nc", day=number)
     mapping_avg_3d(pathToFile="unseen-storm-forecasts/hans_storm/tp24_0.25x0.25_2023.nc", day=number)
+
+### Short testing
+
+""" mapping_avg_3d(pathToFile="unseen-storm-forecasts/hans_storm/tp24_0.25x0.25_2023.nc", day=219) """
