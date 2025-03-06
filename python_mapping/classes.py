2137cb4c16e7e3257972f02c8d745089ac42e394
Émile Sauvat <emile.sauvat@proton.me>
2025-03-06 10:27:31 +0100

    compo init draft 1

--- python_mapping/classes.py
+++ python_mapping/classes.py
@@ -3,8 +3,27 @@
 ###   This is an annex for the weatherdata module
 ###
 
+import os
+
+from weatherdata import *
+
 
 class composite_dataset :
 
-    def __init__(self):
+    def __init__(self, pathToData:str, isDirectory:bool=False):
+        paths = []
+        if isDirectory or pathToData.endswith('/'):
+            for filename in os.listdir(pathToData):
+                paths.append(os.path.join(pathToData, filename))
+        else:
+            paths = open(pathToData, 'r').read().split('\n')
+            if len(paths[-1]) == 0:
+                paths.pop()
+        
+        self.pathList = np.array(paths)
+        self.dims = Dataset(self.pathList[0], 'r').dimensions
+        self.var = Dataset(self.pathList[0], 'r').variables
+
         pass
+
+    
