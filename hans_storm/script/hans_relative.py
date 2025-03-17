2b623a3b8cf7e9502d7591e735ddcca21fe87a98
Émile Sauvat - Sigma2 SSH <emile.sauvat@ens.psl.eu>
2025-03-17 13:44:29 +0100

    Improving colorbar 22

--- hans_storm/script/hans_relative.py
+++ hans_storm/script/hans_relative.py
@@ -56,14 +56,14 @@ for dailyData in hansData:
 
 hansMean = wd.mean_over_time(
     tp2023.sel(time=slice(
-        wd.np.datetime64('2023-08-06'),
+        wd.np.datetime64('2023-08-07'),
         wd.np.datetime64('2023-08-09')
     )), 
     3).sel(time=wd.np.datetime64('2023-08-08'))
 
 res = hansMean / data
 title = "Hans mean precipitations relative to August's max"
-fileName = "tp24_relative-max-august_all-res_hans-06-09"
+fileName = "tp24_relative-max-august_all-res_hans-07-09"
 fig, axis = wd.draw_map(res, title=title, extent=(0,1))
 fig.savefig(endDir+fileName+'.png')
 wd.geo.plt.close()
