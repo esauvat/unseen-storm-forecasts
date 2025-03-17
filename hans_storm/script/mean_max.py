dfa8d92f19a39183263d7da1fe33e0348edca925
Émile Sauvat - Sigma2 SSH <emile.sauvat@ens.psl.eu>
2025-03-17 17:04:20 +0100

    mean maximum over each year

--- hans_storm/script/mean_max.py
+++ hans_storm/script/mean_max.py
@@ -14,7 +14,7 @@ dir = '/nird/projects/NS9873K/emile/unseen-storm-forecasts/hans_storm/data/weath
 with open(path, 'rb') as inp:
     tpSet = pickle.load(inp)
 
-name = 'continuous_max-mean2_0.25_all-time'
+""" name = 'continuous_max-mean2_0.25_all-time'
 if not name in tpSet.compute.keys():
     res = tpSet.compute_time_max(meanSpan = 2)
     endPath = dir + name + '.pkl'
@@ -53,6 +53,46 @@ if not name in tpSet.compute.keys():
         arr = arr.expand_dims({"time":[m]}).transpose("time","latitude","longitude")
         dataarrays.append(arr)
         arr.close()
+    res = wd.xr.concat(dataarrays, "time")
+    del dataarrays
+    endPath = dir + name + '.pkl'
+    with open(endPath, 'wb') as outp:
+        pickle.dump(res, outp, pickle.HIGHEST_PROTOCOL)
+        tpSet.compute[name] = endPath """
+
+name = 'continuous_max-mean2_0.25_annual'
+if not name in tpSet.compute.keys():
+    dataarrays = []
+    for key in tpSet.fileList:
+        data = wd.xr.open_dataarray(tpSet.pathsToFiles[key])
+        arr = wd.xr.full_like(data, wd.np.nan)
+        for i in range(len(arr['time'].values)-1):
+            arr[i,:,:] = data.isel(time=[i,i+1]).mean(dim="time")
+        arr = arr.max(dim="time")
+        arr.expand_dims({"time":[key[1][-4:]]})
+        dataarrays.append(arr)
+        arr.close()
+        data.close()
+    res = wd.xr.concat(dataarrays, "time")
+    del dataarrays
+    endPath = dir + name + '.pkl'
+    with open(endPath, 'wb') as outp:
+        pickle.dump(res, outp, pickle.HIGHEST_PROTOCOL)
+        tpSet.compute[name] = endPath
+
+name = 'continuous_max-mean3_0.25_annual'
+if not name in tpSet.compute.keys():
+    dataarrays = []
+    for key in tpSet.fileList:
+        data = wd.xr.open_dataarray(tpSet.pathsToFiles[key])
+        arr = wd.xr.full_like(data, wd.np.nan)
+        for i in range(1, len(arr['time'].values)-1):
+            arr[i,:,:] = data.isel(time=[i-1,i,i+1]).mean(dim="time")
+        arr = arr.max(dim="time")
+        arr.expand_dims({"time":[key[1][-4:]]})
+        dataarrays.append(arr)
+        arr.close()
+        data.close()
     res = wd.xr.concat(dataarrays, "time")
     del dataarrays
     endPath = dir + name + '.pkl'
